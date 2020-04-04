f48f9845ba034a5f23ba8128f90a493086ab5757
Igor Nikolaev <igor.nikolaev@nikisoft.ru>
2020-04-05 00:41:55 +0300

    Fix iframe host name

--- client/src/LocustView.js
+++ client/src/LocustView.js
@@ -11,7 +11,7 @@ export default (props) => {
         <div style={{padding: '10px'}}>
             <Button onClick={handleClose} color="primary">Close</Button>
         </div>
-      <iframe src={`http://localhost:8000/proxy/${test.id}/`} style={{width: '100%', height: '100%', border: 0}}/>
+      <iframe src={`/proxy/${test.id}/`} style={{width: '100%', height: '100%', border: 0}}/>
     </div>
   );
 };
